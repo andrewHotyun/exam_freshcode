059c0e691b7d4df5a4094a2cd2504b374cfc2c2c
andrewHotyun <kapitansoup200047@gmail.com>
2023-05-08 12:25:47 +0300

    fix dashboard

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -4,17 +4,21 @@ import CONSTANTS from '../../constants';
 import CustomerDashboard from '../../components/CustomerDashboard/CustomerDashboard';
 import CreatorDashboard from '../../components/CreatorDashboard/CreatorDashboard';
 import Header from '../../components/Header/Header';
+import { Redirect } from 'react-router-dom';
 
 const Dashboard = (props) => {
   const { role, history } = props;
+    if (!role) {
+      return <Redirect to="/login"/>
+    }
   return (
     <div>
       <Header />
       {
-                role === CONSTANTS.CUSTOMER
-                  ? <CustomerDashboard history={history} match={props.match} />
-                  : <CreatorDashboard history={history} match={props.match} />
-            }
+        role === CONSTANTS.CUSTOMER
+        ? <CustomerDashboard history={history} match={props.match} />
+        : <CreatorDashboard history={history} match={props.match} />
+      }
     </div>
   );
 };

fb69d38998d798606223512acb45506b3736a312
andrewHotyun <kapitansoup200047@gmail.com>
2023-05-08 14:31:42 +0300

    add in howItWorks component header & footer components

---
+++ client/src/components/HowItWorks/HowItWorks.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./HowItWorks.module.sass"
+import Header from "../Header/Header"
+import Footer from "../Footer/Footer"
+
+const HowItWorks = () => {
+    return (
+        <div>
+        <Header/>
+
+        <Footer/>
+      </div>
+    )
+}
+
+export default HowItWorksComponent;
